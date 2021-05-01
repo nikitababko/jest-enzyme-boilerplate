b2fcf1730ec741efda4106b59ab379d8ff48d834
Nikita Babko <niki-babko@yandex.ru>
2021-05-01 18:44:35 +0700

    Testing rendering.

---
+++ src/Post/post.spec.js
@@ -0,0 +1,29 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import Post from './post';
+
+const setUp = (props) => shallow(<Post {...props} />);
+
+describe('should render Post component', () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it('should contain .post wrapper', () => {
+    const wrapper = component.find('.post');
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('should contain link', () => {
+    const wrapper = component.find('a');
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('should render created date', () => {
+    const created_at = '01-05-2021';
+    component = setUp({ created_at });
+    const date = component.find('.date');
+    expect(date.text()).toBe(new Date(created_at).toLocaleDateString());
+  });
+});
